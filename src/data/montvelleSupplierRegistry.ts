1448fd598be5d7dc78681d5a36fa513bd10e51f7
mandyking308-hub <mandyking308@gmail.com>
2026-08-24 08:48:04 +0100

    Add Montvelle supplier batch 100-03 to registry

--- src/data/montvelleSupplierRegistry.ts
+++ src/data/montvelleSupplierRegistry.ts
@@ -3,6 +3,8 @@ import { MONTVELLE_SUPPLIERS_BATCH_100_01_1 } from "./montvelleSuppliersBatch100
 import { MONTVELLE_SUPPLIERS_BATCH_100_01_2 } from "./montvelleSuppliersBatch100_01b";
 import { MONTVELLE_SUPPLIERS_BATCH_100_02_1 } from "./montvelleSuppliersBatch100_02a";
 import { MONTVELLE_SUPPLIERS_BATCH_100_02_2 } from "./montvelleSuppliersBatch100_02b";
+import { MONTVELLE_SUPPLIERS_BATCH_100_03_1 } from "./montvelleSuppliersBatch100_03a";
+import { MONTVELLE_SUPPLIERS_BATCH_100_03_2 } from "./montvelleSuppliersBatch100_03b";
 
 export const ALL_MONTVELLE_SUPPLIERS: MontvelleSupplier[] = [
   ...MONTVELLE_SUPPLIERS,
@@ -10,6 +12,8 @@ export const ALL_MONTVELLE_SUPPLIERS: MontvelleSupplier[] = [
   ...MONTVELLE_SUPPLIERS_BATCH_100_01_2,
   ...MONTVELLE_SUPPLIERS_BATCH_100_02_1,
   ...MONTVELLE_SUPPLIERS_BATCH_100_02_2,
+  ...MONTVELLE_SUPPLIERS_BATCH_100_03_1,
+  ...MONTVELLE_SUPPLIERS_BATCH_100_03_2,
 ];
 
 export function getMontvelleSupplier(supplierId: string) {
