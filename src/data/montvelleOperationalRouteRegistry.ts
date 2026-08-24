23aecd2ed6c59ae0c151b24bbf5b9c53de5f5ca3
mandyking308-hub <mandyking308@gmail.com>
2026-08-24 08:34:50 +0100

    Add first 100-supplier route batch to Montvelle registry

--- src/data/montvelleOperationalRouteRegistry.ts
+++ src/data/montvelleOperationalRouteRegistry.ts
@@ -3,10 +3,14 @@ import {
   type MontvelleOperationalRoute,
 } from "./montvelleOperationalRoutes";
 import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_02 } from "./montvelleOperationalRoutesBatch02";
+import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_01_1 } from "./montvelleOperationalRoutesBatch100_01a";
+import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_01_2 } from "./montvelleOperationalRoutesBatch100_01b";
 
 export const ALL_MONTVELLE_OPERATIONAL_ROUTES: MontvelleOperationalRoute[] = [
   ...MONTVELLE_OPERATIONAL_ROUTES,
   ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_02,
+  ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_01_1,
+  ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_01_2,
 ];
 
 export function getAllMontvelleOperationalRoutes(supplierId: string) {
