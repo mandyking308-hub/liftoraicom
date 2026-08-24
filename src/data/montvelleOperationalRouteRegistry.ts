cf25596608f01c4eeec113b51a6121b91477034f
mandyking308-hub <mandyking308@gmail.com>
2026-08-24 09:24:57 +0100

    Register Montvelle operational routes batch 004

--- src/data/montvelleOperationalRouteRegistry.ts
+++ src/data/montvelleOperationalRouteRegistry.ts
@@ -9,6 +9,8 @@ import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_02_1 } from "./montvelleOperatio
 import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_02_2 } from "./montvelleOperationalRoutesBatch100_02b";
 import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_03_1 } from "./montvelleOperationalRoutesBatch100_03a";
 import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_03_2 } from "./montvelleOperationalRoutesBatch100_03b";
+import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_04_1 } from "./montvelleOperationalRoutesBatch100_04a";
+import { MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_04_2 } from "./montvelleOperationalRoutesBatch100_04b";
 
 export const ALL_MONTVELLE_OPERATIONAL_ROUTES: MontvelleOperationalRoute[] = [
   ...MONTVELLE_OPERATIONAL_ROUTES,
@@ -19,6 +21,8 @@ export const ALL_MONTVELLE_OPERATIONAL_ROUTES: MontvelleOperationalRoute[] = [
   ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_02_2,
   ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_03_1,
   ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_03_2,
+  ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_04_1,
+  ...MONTVELLE_OPERATIONAL_ROUTES_BATCH_100_04_2,
 ];
 
 export function getAllMontvelleOperationalRoutes(supplierId: string) {
